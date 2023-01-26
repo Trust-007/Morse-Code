8f38679a466bfb6dadfccbac3849f851af7f0111
Trust7 <simeontrust7@gmail.com>
2023-01-26 10:51:42 +0100

    Updated README and fixed linters

--- decoder.rb
+++ decoder.rb
@@ -1,41 +1,33 @@
 def decode_char(char)
   morse = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---',
-         '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..']
-alphabets = %w[A B C D E F G H I J K L M N O P Q R S T U
-               V W X Y Z]
+           '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..']
+  alphabets = %w[A B C D E F G H I J K L M N O P Q R S T U
+                 V W X Y Z]
   morse.each_with_index do |element, index|
-    if char == element
-      return alphabets[index]
-    end
+    return alphabets[index] if char == element
   end
 end
 
 def decode_word(word)
-  decod_word = ""
-  char = word.split(" ")
-  char.each do |char|
+  decod_word = ''
+  character = word.split
+  character.each do |char|
     decod_word += decode_char(char)
   end
   decod_word
 end
 
 def decode(string)
-  decod_string = ""
-  string = string.split("  ")
+  decod_string = ''
+  string = string.split('  ')
   string.each_with_index do |word, index|
-    if(index == string.length-1)
-      decod_string += decode_word(word) 
-    else
-      decod_string += decode_word(word) + " "  
-    end    
+    decod_string += if index == string.length - 1
+                      decode_word(word)
+                    else
+                      "#{decode_word(word)} "
+                    end
   end
   decod_string
 end
 
-print decode_char('.--')
-puts
-print decode_word('-- -.--')
-
-print decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
-puts
-
+decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
