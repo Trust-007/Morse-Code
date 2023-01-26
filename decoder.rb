22f03bfdfed09f609fbb2416f44ff379365e0d79
Mujeeb4582 <mujeeburahman4582@gmail.com>
2023-01-26 12:17:12 +0500

    Add the method to read a word from morse code

--- decoder.rb
+++ decoder.rb
@@ -1,15 +1,25 @@
-morse = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---',
+def decode_char(char)
+  morse = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---',
          '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..']
 alphabets = %w[A B C D E F G H I J K L M N O P Q R S T U
                V W X Y Z]
-
-def decode_char(char, morse, alphabets)
   morse.each_with_index do |element, index|
     if char == element
-      puts alphabets[index]
-      break
+      return alphabets[index]
     end
   end
 end
 
-print decode_char('.--', morse, alphabets)
+def decode_word(word)
+  decod_word = ""
+  char = word.split(" ")
+  char.each do |char|
+    decod_word += decode_char(char)
+  end
+  decod_word
+end
+
+print decode_char('.--')
+puts
+print decode_word('-- -.--')
+
